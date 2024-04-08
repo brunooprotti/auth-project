f8275549ff350dbea751069527e3199b58fe8cd9
Bruno Protti <brunooprotti@gmail.com>
2024-04-08 19:49:01 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using auth_backend.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace auth_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // GET: api/<AuthController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AuthController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AuthController>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public void Post([FromBody] CreateUserDto value)
+        {
+        }
+
+        // PUT api/<AuthController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AuthController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+using AspNetCore.Identity.MongoDbCore.Infrastructure;
+using auth_backend.DAL.Model;
+using auth_backend.mongo;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Connection string for mongodb
+
+//var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();//El GetSection(nameof(MongoDbConfig)) busca el nombre de la clase en el app settings y el Get lo convierte al tipo
+//var asd = builder.Configuration.GetSection("MongoDbConfig");
+//builder.Services.Configure<MongoDbConfig>( builder.Configuration.GetSection("MongoDbConfig") );
+
+//builder.Services.AddSingleton<IMongoClient>(sp =>
+//{
+//    var config = sp.GetRequiredService<IOptions<MongoDbConfig>>().Value;
+//    return new MongoClient(config.ConnectionString);
+//});
+
+var mongoDBSettings = builder.Configuration.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection("MongoDBSettings"));
+
+builder.Services.AddDbContext<CarBookingDbContext>(options =>
+options.UseMongoDB(mongoDBSettings.AtlasURI ?? "", mongoDBSettings.DatabaseName ?? ""));
+
+
+////Se agrega Identity
+//builder.Services.AddIdentity<User, Roles>()
+//    .AddMongoDbStores<User, Roles, Guid>( mongoDbSettings.ConnectionString , mongoDbSettings.Name );
+
+////Se Agrega AutoMapper
+//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+//builder.Services.Configure<>
+
+//Connection string for sqlserver
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration[]);
+//});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
