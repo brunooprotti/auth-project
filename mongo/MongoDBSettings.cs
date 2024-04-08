26e6be0d9a4043b1caaa6f0ec17cb4bb93ca914e
Bruno Protti <brunooprotti@gmail.com>
2024-04-08 19:49:01 -0300

    Agregar archivos de proyecto.

---
+++ mongo/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace auth_backend.mongo
+{
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+    }
+}
